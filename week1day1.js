b94bfb33f0c14d4a0665c6a0947e107fb73af8a0
Mark Edelman <markedelman1987@gmail.com>
2016-12-02 22:11:06 -0500

    zeus age bc

--- week1day1.js
+++ week1day1.js
@@ -1,9 +1,23 @@
-function whoAmI(){
+function whoAmI(name, age){
+// var name = 'Schwartznegger';
+// var age = 'unknown';
 
-console.log("you can use mary's bike");
+var yearOfBirth = 2016 - age;
 
+if (yearOfBirth < 0) {
+	return yearOfBirth * (-1) + "B.C."; 
+}
+
+console.log("You can use " + name + "'s bike, and " + name + " is " +
+ age + " years old. " + name + " was born in " + yearOfBirth + ".");
 
 }
 
+whoAmI("Zeus", 2016);
 
-whoAmI();
+// funct..........(name, age){ 
+//   var yearOf..........16 - age;
+//   console.l..........my name is Chris and I'm 29 ye..........
+//   console.log("I was born in " + yearOf.......
+// }
+// whoAmI(..........);
